3c6deac195edbe236929bc45369a07fd696b5160
Muhammad Bin Umair <81143461+MuhammadBinUmair@users.noreply.github.com>
2026-01-28 09:50:19 +0500

    Add files via upload

---
+++ OOP OEL/Name and University.cpp
@@ -0,0 +1,429 @@
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+#include "Muhammad.h"
+using namespace std;
+int main()
+{  
+// Line 1: MUHAMMAD BIN UMAIR
+// MUHAMMAD:
+// M
+for(int i=2,j=12;j>=2;j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=2,j=2; i<=7 && j<=7; i++,j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=7,j=7; i<=12 && j>=2; i++,j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=12,j=2; j<=12; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// U
+for(int i=15,j=2; j<=11; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=16,j=12; i<=23; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=24,j=11; j>=2; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// H
+for(int i=27,j=2; j<=12; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=28,j=7; i<=34; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=35,j=2; j<=12; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// A
+for(int i=38,j=12; j>=2; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=39,j=2; i<=45; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=46,j=3; j<=12; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=39,j=7; i<=45; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// M
+for(int i=49,j=12; j>=2; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=49,j=2; i<=54 && j<=7; i++,j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=54,j=7; i<=59 && j>=2; i++,j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=59,j=2; j<=12; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// M
+for(int i=62,j=12; j>=2; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=62,j=2; i<=67 && j<=7; i++,j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=67,j=7; i<=72 && j>=2; i++,j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=72,j=2; j<=12; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// A
+for(int i=75,j=12; j>=2; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=76,j=2; i<=82; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=83,j=3; j<=12; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=76,j=7; i<=82; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// D
+for(int i=86,j=2; j<=12; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=87,j=2; i<=92; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=93,j=3; j<=11; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=92,j=12; i>=87; i--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// BIN:
+// B
+for(int i=100,j=12; j>=2; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=101,j=2; i<=106; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=107,j=3; j<=6; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=106,j=7; i>=101; i--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=107,j=8; j<=11; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=106,j=12; i>=101; i--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// I
+for(int i=113,j=2; j<=12; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=110,j=2; i<=116; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=110,j=12; i<=116; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// N
+for(int i=119,j=12; j>=2; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=119,j=2; i<=127 && j<=12; i++, j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=127,j=12; j>=2; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// UMAIR:
+// U
+for(int i=135,j=2; j<=11; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=136,j=12; i<=143; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=144,j=11; j>=2; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// M
+for(int i=147,j=12; j>=2; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=147,j=2; i<=152 && j<=7; i++,j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=152,j=7; i<=157 && j>=2; i++,j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=157,j=2; j<=12; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// A
+for(int i=160,j=12; j>=2; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=161,j=2; i<=167; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=168,j=3; j<=12; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=161,j=7; i<=167; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// I
+for(int i=174,j=2; j<=12; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=171,j=2; i<=177; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=171,j=12; i<=177; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// R
+for(int i=180,j=12; j>=2; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=181,j=2; i<=186; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=187,j=3; j<=6; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=186,j=7; i>=181; i--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=183,j=7; i<=188 && j<=12; i++, j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// LINE 2: FOUNDATION UNIVERSITY
+// FOUNDATION:
+// F
+for(int i=2,j=24; j>=14; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=2,j=14; i<=9; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=2,j=19; i<=7; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// O
+for(int i=13, j=14; i<=18; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=12, j=15; j<=23; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=13, j=24; i<=18; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=19, j=15; j<=23; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// U
+for(int i=22,j=14; j<=23; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=23,j=24; i<=29; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=30,j=23; j>=14; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// N
+for(int i=33,j=24; j>=14; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=33,j=14; i<=41 && j<=24; i++, j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=41,j=24; j>=14; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// D
+for(int i=44,j=14; j<=24; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=45,j=14; i<=49; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=50,j=15; j<=23; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=49,j=24; i>=45; i--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// A
+for(int i=53,j=24; j>=14; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=54,j=14; i<=60; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=61,j=14; j<=24; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=54,j=19; i<=60; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// T
+for(int i=64,j=14; i<=72; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=68,j=15; j<=24; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// I
+for(int i=77,j=14; j<=24; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=74,j=14; i<=80; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=74,j=24; i<=80; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// O
+for(int i=84, j=14; i<=89; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=83, j=15; j<=23; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=84, j=24; i<=89; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=90, j=15; j<=23; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// N
+for(int i=93,j=24; j>=14; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=93,j=14; i<=101 && j<=24; i++, j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=101,j=24; j>=14; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// UNIVERSITY:
+// U
+for(int i=111,j=14; j<=23; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=112,j=24; i<=118; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=119,j=23; j>=14; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// N
+for(int i=122,j=24; j>=14; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=122,j=14; i<=130 && j<=24; i++, j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=130,j=24; j>=14; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// I
+for(int i=132,j=14; i<=138; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=135,j=15; j<=23; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=132,j=24; i<=138; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// V
+for(int i=141,j=14; i<=151 && j<=24; i++, j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=152,j=23; i<=162 && j>=14; i++, j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// E
+for(int i=165,j=24; j>=14; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=166,j=14; i<=173; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=166,j=19; i<=171; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=166,j=24; i<=173; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// R
+for(int i=176,j=24; j>=14; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=177,j=14; i<=182; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=183,j=15; j<=18; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=182,j=19; i>=177; i--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=179,j=19; i<=184 && j<=24; i++, j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// S
+for(int i=194,j=14; i>=187; i--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=187,j=15; j<=18; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=188,j=19; i<=194; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=194,j=20; j<=23; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=193,j=24; i>=187; i--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// I
+for(int i=197,j=14; i<=203; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=200,j=15; j<=23; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=197,j=24; i<=203; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// T
+for(int i=206,j=14; i<=214; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=210,j=15; j<=24; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// Y
+for(int i=217,j=14; j<=18; i++,j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=225,j=14; j<=18; i--,j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=221,j=19; j<=24; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// Line 3: SCHOOL OF SCIENCE:
+// SCHOOL:
+// S
+for(int i=10, j=26; i>=2; i--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=2, j=27; j<=29; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=3, j=30; i<=10; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=10, j=31; j<=33; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=9, j=34; i>=2; i--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// C
+for(int i=13, j=27; j<=33; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=14, j=26; i<=20; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=14, j=34; i<=20; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// H
+for(int i=23, j=26; j<=34; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=24, j=30; i<=29; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=30, j=26; j<=34; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// O
+for(int i=33, j=27; j<=33; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=40, j=27; j<=33; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=34, j=26; i<=39; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=34, j=34; i<=39; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// O
+for(int i=43, j=27; j<=33; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=50, j=27; j<=33; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=44, j=26; i<=49; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=44, j=34; i<=49; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// L
+for(int i=53, j=26; j<=34; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=54, j=34; i<=60; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// OF
+// O
+for(int i=76, j=26; i<=81; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=75, j=27; j<=33; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=76, j=34; i<=81; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=82, j=27; j<=33; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// F
+for(int i=86, j=26; j<=34; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=87, j=26; i<=93; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=87, j=30; i<=91; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// SCIENCE:
+// S
+for(int i=113, j=26; i>=105; i--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=105, j=27; j<=29; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=106, j=30; i<=113; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=113, j=31; j<=33; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=112, j=34; i>=105; i--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// C
+for(int i=116, j=27; j<=33; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=117, j=26; i<=123; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=117, j=34; i<=123; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// I
+for(int i=127, j=26; j<=34; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=125, j=26; i<=129; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=125, j=34; i<=129; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// E
+for(int i=132, j=26; j<=34; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=133, j=26; i<=139; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=133, j=30; i<=137; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=133, j=34; i<=139; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// N
+for(int i=142, j=34; j>=26; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=142, j=26; i<=150; i++, j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=150, j=34; j>=26; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// C
+for(int i=153, j=27; j<=33; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=154, j=26; i<=160; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=154, j=34; i<=160; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// E
+for(int i=163, j=26; j<=34; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=164, j=26; i<=170; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=164, j=30; i<=168; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=164, j=34; i<=170; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+   
+// Line 4: AND TECHNOLOGY:
+// AND:
+// A
+for(int i=2, j=44; j>=36; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=2, j=36; i<=9; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=9, j=36; j<=44; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=2, j=40; i<=9; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// N
+for(int i=12, j=44; j>=36; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=12, j=36; i<=20; i++, j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=20, j=44; j>=36; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// D
+for(int i=23, j=36; j<=44; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=23, j=36; i<=28; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=29, j=37; j<=43; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=28, j=44; i>=23; i--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// TECHNOLOGY:
+// T
+for(int i=40, j=36; i<=48; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=44, j=36; j<=44; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// E
+for(int i=51, j=36; j<=44; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=52, j=36; i<=58; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=52, j=40; i<=56; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=52, j=44; i<=58; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// C
+for(int i=61, j=37; j<=43; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=62, j=36; i<=68; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=62, j=44; i<=68; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// H
+for(int i=71, j=36; j<=44; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=72, j=40; i<=77; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=78, j=36; j<=44; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// N
+for(int i=81, j=44; j>=36; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=81, j=36; i<=89; i++, j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=89, j=44; j>=36; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// O
+for(int i=93, j=36; i<=98; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=92, j=37; j<=43; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=93, j=44; i<=98; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=99, j=37; j<=43; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// L
+for(int i=102, j=36; j<=44; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=103, j=44; i<=109; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// O
+for(int i=113, j=36; i<=118; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=112, j=37; j<=43; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=113, j=44; i<=118; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=119, j=37; j<=43; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// G
+for(int i=122, j=37; j<=43; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=123, j=36; i<=129; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=123, j=44; i<=129; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=129, j=40; j<=43; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=126, j=40; i<=129; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// Y
+for(int i=132, j=36; j<=40; i++, j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=140, j=36; j<=40; i--, j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=136, j=41; j<=44; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+   
+// Line 5: ISLAMABAD
+// I
+for(int i=5, j=46; j<=54; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=2, j=46; i<=8; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=2, j=54; i<=8; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// S
+for(int i=19, j=46; i>=11; i--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=11, j=47; j<=49; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=12, j=50; i<=19; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=19, j=51; j<=53; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=18, j=54; i>=11; i--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// L
+for(int i=22, j=46; j<=54; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=23, j=54; i<=29; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// A
+for(int i=32, j=54; j>=46; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=33, j=46; i<=39; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=40, j=46; j<=54; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=33, j=50; i<=39; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// M
+for(int i=43, j=54; j>=46; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=44, j=47; i<=48; i++, j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=49, j=50; i<=52; i++, j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=53, j=46; j<=54; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// A
+for(int i=56, j=54; j>=46; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=57, j=46; i<=63; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=64, j=46; j<=54; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=57, j=50; i<=63; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// B
+for(int i=67, j=46; j<=54; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=68, j=46; i<=73; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=68, j=50; i<=73; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=68, j=54; i<=73; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=74, j=47; j<=49; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=74, j=51; j<=53; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// A
+for(int i=77, j=54; j>=46; j--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=78, j=46; i<=84; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=85, j=46; j<=54; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=78, j=50; i<=84; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+
+// D
+for(int i=88, j=46; j<=54; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=89, j=46; i<=93; i++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=94, j=47; j<=53; j++) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+for(int i=93, j=54; i>=89; i--) { gotoxy(i,j); cout<<"*"; Sleep(10); }
+getch();
+}
